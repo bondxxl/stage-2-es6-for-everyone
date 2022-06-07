51f421546c89d2f52332fabfe30f73854041b5ad
bondxxl <bondxxl01@gmail.com>
2022-06-07 22:01:00 +0300

    Implemented

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,6 +2,7 @@ import { createElement } from '../helpers/domHelper';
 import { renderArena } from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import { fighterService } from '../services/fightersService';
 
 export function createFightersSelector() {
   let selectedFighters = [];
@@ -20,6 +21,11 @@ export function createFightersSelector() {
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
+  let fighterDetails;
+  return fighterDetailsMap.has(fighterId)
+      ? fighterDetailsMap.get(fighterId)
+      : (fighterDetailsMap.set(fighterId, fighterDetails = await fighterService.getFighterDetails(fighterId))
+          , fighterDetails);
   // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
 }
 
