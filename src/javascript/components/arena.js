a953d3bfff7545f2ea3367bccc78672ae00493b8
bondxxl <bondxxl01@gmail.com>
2022-06-08 19:34:00 +0300

    Created getActions function

--- src/javascript/components/arena.js
+++ src/javascript/components/arena.js
@@ -1,5 +1,8 @@
 import { createElement } from '../helpers/domHelper';
 import { createFighterImage } from './fighterPreview';
+import {fight} from "./fight";
+
+
 
 export function renderArena(selectedFighters) {
   const root = document.getElementById('root');
@@ -11,6 +14,8 @@ export function renderArena(selectedFighters) {
   // todo:
   // - start the fight
   // - when fight is finished show winner
+  fight(...selectedFighters)
+      .then(data => console.log(data)).catch(error => console.error(error));
 }
 
 function createArena(selectedFighters) {
--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,8 +1,70 @@
 import { controls } from '../../constants/controls';
+// import {showWinnerModal} from "modal/winner";
 
 export async function fight(firstFighter, secondFighter) {
+
+  function isKeyIncludes(key, controls, keysSet) {
+    return typeof controls[key] === 'object'
+        ? controls[key].every(el => keysSet.has(el))
+        : keysSet.has(controls[key]);
+  }
+
+  function populateActionSets(ev, keysPressed, keysPressed2) {
+    let control = Object.keys(controls)
+        .find(key => {
+          return typeof controls[key] === 'object'
+              ? controls[key].includes(ev.code)
+              : controls[key] === ev.code;
+        });
+    if (control && control.includes('One')) {
+      keysPressed.add(ev.code);
+    } else if (control && control.includes('Two')) {
+      keysPressed2.add(ev.code);
+    }
+  }
+
+  function getActions(keysPressed, keysPressed2) {
+    /*
+    * 1. if player1 have action and player2 have action then break
+    * 2. if player1 haven't action then init or stay undefined
+    * 3. else if player2 haven't action then init or stay undefined
+    *
+    * before the iteration set we reverse it, because critical hit has the highest priority
+    * */
+    let pOneAction;
+    let pTwoAction;
+    for (const key of Array.from(Object.keys(controls)).reverse()) {
+      if (pOneAction && pTwoAction) {
+        break;
+      }
+      if (!pOneAction && isKeyIncludes(key, controls, keysPressed)) {
+        pOneAction = key;
+      }
+      else if (!pTwoAction && isKeyIncludes(key, controls, keysPressed2)) {
+        pTwoAction = key;
+      }
+    }
+    return [pOneAction, pTwoAction];
+  }
+
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+    const keysPressed = new Set();
+    const keysPressed2 = new Set();
+    window.addEventListener('keydown', ev => {
+      populateActionSets(ev, keysPressed, keysPressed2);
+    });
+
+    window.addEventListener('keyup', ev => {
+      if (!keysPressed.size && !keysPressed2.size) {
+        return;
+      }
+
+      console.log(getActions(keysPressed, keysPressed2));
+
+      keysPressed.clear();
+      keysPressed2.clear();
+    });
   });
 }
 
